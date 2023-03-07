fbd553fe5ee003ef552d6bc8e645e3e2621da9dd
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-03-07 23:51:52 +0100

    added

--- with-tailwindcss-app/pages/index.tsx
+++ with-tailwindcss-app/pages/index.tsx
@@ -7,23 +7,58 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="flex justify-center items-center">
-        <form className="mt-5">
-          <input
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            placeholder="Enter contract address.."
-            value={contractAddress}
-            onChange={(e) => setContractAddress(e.target.value)}
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-6 rounded-full"
-            onClick={() => Router.push(`/contracts/${contractAddress}`)}
+<div className="bg-white py-16 sm:py-24">
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
+          <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Get notified when we’re launching.
+          </h2>
+          <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
+            Reprehenderit ad esse et non officia in nulla. Id proident tempor incididunt nostrud nulla et culpa.
+          </p>
+          <form className="mx-auto mt-10 flex max-w-md gap-x-4">
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
+            <input
+             
+              className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+              placeholder="Enter contract address.."
+              value={contractAddress}
+              onChange={(e) => setContractAddress(e.target.value)}
+            />
+            <button
+              type="submit"
+              onClick={() => Router.push(`/contracts/${contractAddress}`)}
+              className="flex-none rounded-md bg-white py-2.5 px-3.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            >
+              Submit
+            </button>
+          </form>
+          <svg
+            viewBox="0 0 1024 1024"
+            className="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
+            aria-hidden="true"
           >
-            Submit
-          </button>
-        </form>
+            <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+            <defs>
+              <radialGradient
+                id="759c1415-0410-454c-8f7c-9a820de03641"
+                cx={0}
+                cy={0}
+                r={1}
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(512 512) rotate(90) scale(512)"
+              >
+                <stop stopColor="#7775D6" />
+                <stop offset={1} stopColor="#E935C1" stopOpacity={0} />
+              </radialGradient>
+            </defs>
+          </svg>
+        </div>
       </div>
+    </div>
+
     </>
   );
 };
