8c0007cbdc04e633baa5edf6b208bcd5bdbf59bc
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-05-14 23:22:06 +0200

    Fixed Footer on Explorer page

--- with-tailwindcss-app/components/Footer.tsx
+++ with-tailwindcss-app/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+
 const navigation = [
   {
     name: "Facebook",
@@ -63,6 +65,32 @@ const navigation = [
 ];
 
 export const Footer = () => {
+  const router = useRouter();
+  if (router.route === "/explorer") {
+    return (
+      <footer className="bg-sky-900">
+        <div className="mx-auto max-w-7xl py-2 px-6 md:flex md:items-center md:justify-between lg:px-8">
+          <div className="flex justify-center space-x-6 md:order-2">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-gray-100 hover:text-gray-300"
+              >
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))}
+          </div>
+          <div className="mt-8 md:order-1 md:mt-0">
+            <p className="text-center text-xs leading-5 text-gray-100">
+              &copy; Copyright © 2023 Project. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+    );
+  }
   return (
     <footer className="bg-sky-100">
       <div className="mx-auto max-w-7xl py-2 px-6 md:flex md:items-center md:justify-between lg:px-8">
