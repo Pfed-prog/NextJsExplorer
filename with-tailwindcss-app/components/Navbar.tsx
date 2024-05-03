b8a1fc6486433dcfb146994c1112e63309e51405
name <fedorovn19@gmail.com>
2024-05-03 14:01:51 +0200

    upgraded icons

--- with-tailwindcss-app/components/Navbar.tsx
+++ with-tailwindcss-app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Disclosure } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 import explore from "/public/explore.png";
@@ -23,9 +23,9 @@ export const Navbar = () => {
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
--- with-tailwindcss-app/services/wagmiConfig.ts
+++ with-tailwindcss-app/services/wagmiConfig.ts
@@ -10,4 +10,5 @@ export const wagmiConfig = getDefaultConfig({
     [mainnet.id]: http(),
     [optimism.id]: http(),
   },
+  ssr: true,
 });
