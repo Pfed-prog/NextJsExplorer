1686b28d562f30f0fd0ea0c1fbaac6dccae9dd84
name <fedorovn19@gmail.com>
2024-05-06 20:52:12 +0200

    correcting button on homepage and removing unused css

--- with-tailwindcss-app/pages/index.tsx
+++ with-tailwindcss-app/pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
             />
             <Link
               href={`/contracts/${contractAddress}`}
-              className="flex-none rounded-md bg-white py-2.5 px-3.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              className="flex-none rounded-md bg-white py-2.5 px-3.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
               Submit
             </Link>
