744f8f6501ea34d767022f72ec9a96bd34fc3e9f
name <fedorovn19@gmail.com>
2024-05-19 16:27:56 +0200

    improving explorer page

--- with-tailwindcss-app/pages/explore/[page].tsx
+++ with-tailwindcss-app/pages/explore/[page].tsx
@@ -1,31 +1,16 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
 
 import { ContractListItem } from "@/components/ContractListItem";
 import { PageSEO } from "@/components/SEO";
-import { getProject, Project } from "@/services/ProjectService";
-import { LocalContract } from "@/services/ProjectService";
+import { getProject, LocalContract } from "@/services/ProjectService";
 
 const Explorer: NextPage = () => {
   const router = useRouter();
   const { page } = router.query;
 
-  const [project, setProject] = useState<Project>();
-  const [mounted, setHasMounted] = useState<boolean>(false);
-
-  const fetchProject = async () => {
-    if (page) {
-      const project = getProject(page as string);
-      setProject(project);
-    }
-  };
-
-  useEffect(() => {
-    fetchProject();
-    setHasMounted(true);
-  }, [page]);
+  const project = getProject(page as string);
 
   const contractListItems = project?.contracts.map(
     (contract: LocalContract) => (
@@ -40,17 +25,15 @@ const Explorer: NextPage = () => {
       <div className="p-2">
         <h2 className="text-3xl mt-5 font-semibold">{project?.name}</h2>
 
-        {mounted && (
-          <div className="mt-10 mx-auto flex items-center justify-center">
-            <Image
-              src={"/" + project?.logoPath}
-              alt={project?.name ?? ""}
-              width={100}
-              height={200}
-              priority
-            />
-          </div>
-        )}
+        <div className="mt-10 mx-auto flex items-center justify-center">
+          <Image
+            src={"/" + project?.logoPath}
+            alt={project?.name ?? ""}
+            width={100}
+            height={200}
+            priority
+          />
+        </div>
 
         <table className="flex items-center justify-center border-separate border-spacing-y-3 mt-5">
           <tbody>{contractListItems}</tbody>
