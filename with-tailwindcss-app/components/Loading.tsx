29f7684248ea6e00ab004287355703668700d153
name <fedorovn19@gmail.com>
2024-05-19 13:39:17 +0200

    fixing loading component

--- with-tailwindcss-app/components/Loading.tsx
+++ with-tailwindcss-app/components/Loading.tsx
@@ -1,11 +1,7 @@
 export const Loading = () => {
   return (
-    <div className="h-screen bg-sky-100">
-      <div className="flex justify-center items-center h-full">
-        <div className="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 ">
-          <div className="border-t-transparent border-solid animate-spin rounded-full border-blue-400 border-8 h-64 w-64" />
-        </div>
-      </div>
+    <div className="flex justify-center items-center">
+      <div className="border-t-transparent border-solid animate-spin rounded-full border-blue-400 border-8 h-64 w-64" />
     </div>
   );
 };
