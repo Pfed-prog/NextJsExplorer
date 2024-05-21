4a1f25f0abd95d844991a2d235e9975f363e7d33
name <fedorovn19@gmail.com>
2024-05-21 13:16:03 +0200

    added image on token

--- with-tailwindcss-app/components/BalanceCard.tsx
+++ with-tailwindcss-app/components/BalanceCard.tsx
@@ -2,6 +2,7 @@ import { formatEther } from "viem";
 
 import { type AddressInfo } from "@/hooks/blockscout/queries";
 import { parseHash } from "@/utils/hashes";
+import Image from "next/image";
 
 interface ContractProps {
   address: `0x${string}`;
@@ -18,6 +19,16 @@ export const BalanceCard = (props: ContractProps) => {
   const etherValue = formatEther(BigInt(addressInfo?.coin_balance));
   return (
     <div className="fade-in-1s items-center justify-center max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto font-semibold mt-2 rounded-lg drop-shadow-md bg-gray-50 sm:p-6 mb-8">
+      {addressInfo.token?.icon_url && (
+        <Image
+          src={addressInfo.token?.icon_url}
+          height={36}
+          width={36}
+          alt={addressInfo.token?.name}
+          className="mx-auto mb-2 rounded-sm"
+        />
+      )}
+
       <div className="break-all text-3xl sm:text-4xl font-semibold">
         {addressInfo.token?.name ??
           addressInfo?.implementation_name ??
--- with-tailwindcss-app/next.config.js
+++ with-tailwindcss-app/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.coingecko.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
 };
