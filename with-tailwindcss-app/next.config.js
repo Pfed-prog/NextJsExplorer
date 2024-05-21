966b9045d4d3e0869d17f00a803adb2e9f3203d6
name <fedorovn19@gmail.com>
2024-05-21 21:32:09 +0200

    fixing balance card; initial empty address redirects to poap

--- with-tailwindcss-app/next.config.js
+++ with-tailwindcss-app/next.config.js
@@ -15,4 +15,14 @@ module.exports = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/contracts/mainnet",
+        destination:
+          "/contracts/mainnet/0x22C1f6050E56d2876009903609a2cC3fEf83B415",
+        permanent: true,
+      },
+    ];
+  },
 };
