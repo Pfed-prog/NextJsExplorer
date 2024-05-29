cec4f029ee3d4dc231f802f94097ffe0c952fe91
name <fedorovn19@gmail.com>
2024-05-29 19:35:43 +0200

    improving camel case

--- with-tailwindcss-app/utils/parseNames.ts
+++ with-tailwindcss-app/utils/parseNames.ts
@@ -1,6 +1,8 @@
-export const camelToFlat = (c: string) => (
-  (c = c.replace(/(?<=[a-z])([A-Z])/g, " $1")), c[0].toUpperCase() + c.slice(1)
-);
+export function camelToFlat(c: string) {
+  c = c.replace(/([a-z])([A-Z0-9])/g, "$1 $2");
+  c = c.replace(/(\d+)$/, " $1").replace(/(\d+)(\D)/g, "$1 $2");
+  return c;
+}
 
 export function parseCamelCase(name: string | null | undefined) {
   if (name) return camelToFlat(name);
