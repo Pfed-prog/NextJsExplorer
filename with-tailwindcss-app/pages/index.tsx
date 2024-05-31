81398502505d6a7ab6c822689d6a9a89d7d34c5f
name <fedorovn19@gmail.com>
2024-05-31 22:58:30 +0200

    improving design;

--- with-tailwindcss-app/pages/index.tsx
+++ with-tailwindcss-app/pages/index.tsx
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
   return (
     <div>
       <PageSEO />
-      <div className="items-center justify-center bg-sky-100">
+      <div className="items-center justify-center bg-sky-200">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
             <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
