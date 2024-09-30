b4afc8bd43c3365084acc915ad3f905a4c0ac092
name <fedorovn19@gmail.com>
2024-10-01 00:49:03 +0200

    updated meta

--- with-tailwindcss-app/pages/_document.tsx
+++ with-tailwindcss-app/pages/_document.tsx
@@ -6,6 +6,10 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="UTF-8" />
+          <meta
+            name="ahrefs-site-verification"
+            content="0b854a10ab949834888dd13297f5c602a02ab526ee2f999f985e4b128740dd26"
+          />
           <link rel="icon" href="/search.svg" />
         </Head>
         <body className="bg-sky-200">
